830d82c37a0c9646766f163c026be5557b099d04
Patrick Brett <pkbrett37@gmail.com>
2019-08-23 12:30:57 +1000

    Basic fetch request frontend->backend->Github

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const fetch = require('node-fetch')
+const app = express()
+app.use(cors())
+
+app.get('/', function (req, res) {
+  const response = { cool: { have: "fun" }}
+
+  res.json(response)
+})
+
+app.get('/repo', function (req, res) {
+  const { owner, repo } = req.query
+
+  fetch(`https://api.github.com/repos/${owner}/${repo}`).then(fetchRes => {
+    fetchRes.json().then(fetchJson => {
+      console.log(fetchJson)
+
+      res.json(fetchJson)
+    })
+  })
+})
+
+app.listen(3000)
