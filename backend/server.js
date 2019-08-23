3ea68f86f085ab068bd0e8cecb26ae55a5350c7e
Patrick Brett <pkbrett37@gmail.com>
2019-08-23 13:15:02 +1000

    Implement Github webhook receiver

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 const fetch = require('node-fetch')
+const bodyParser = require('body-parser')
 const app = express()
 app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
   const response = { cool: { have: "fun" }}
@@ -20,6 +22,16 @@ app.get('/repo', function (req, res) {
       res.json(fetchJson)
     })
   })
+})
+
+app.post('/github', function(req, res) {
+  const { headers, body } = req
+
+  console.log("body", body)
+  console.log("header", headers)
+
+  res.json({})
+  res.status(200)
 })
 
 app.listen(3000)
