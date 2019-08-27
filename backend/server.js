baeb0c5f01c333f9cb17fa2dc3db6f7c6d197b7b
Sara Uyen Tran <utra0001@student.monash.edu>
2019-08-27 20:18:43 +1000

    Add basic mail function sendMail()
    Need to configure sender, receiver, email content and template

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const fetch = require('node-fetch')
 const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer');
+
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
@@ -33,5 +35,35 @@ app.post('/github', function(req, res) {
   res.json({})
   res.status(200)
 })
+
+function sendMail(receivers, emailContent){
+  // Source: https://nodemailer.com/about/
+  /* TODO: 
+  - configure SMTP transport (do we need to get an SMTP server or just use a regular account)
+  - configure sender, receiver, email content
+   */
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: testAccount.user, // TODO
+        pass: testAccount.pass // TODO
+    }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: 'bar@example.com, baz@example.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world?', // plain text body
+    html: '<b>Hello world?</b>' // html body
+  });
+
+  console.log('Message sent: %s', info.messageId);
+};
 
 app.listen(3000)
